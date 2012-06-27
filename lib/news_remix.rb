9ef6364c4fd01da7bc97239a480c8de637191108
TM Lee <tmlee@TM-Lees-MacBook-Pro.local>
2012-06-28 01:21:12 +0800

    require files

--- lib/news_remix.rb
+++ lib/news_remix.rb
@@ -1,5 +1,8 @@
+require 'curb'
+require 'json'
+require 'uri'
 require "news_remix/version"
-
-module NewsRemix
-  # Your code goes here...
-end
+require "news_remix/author"
+require "news_remix/story_element"
+require "news_remix/news_element"
+require "news_remix/remix"
